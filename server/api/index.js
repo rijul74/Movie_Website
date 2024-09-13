9b4c7b51fe470ba90cb193d96b39c4585c688e5d
rijul74 <76898528+rijul74@users.noreply.github.com>
2024-09-13 17:15:11 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -6,7 +6,12 @@ require('dotenv').config();
 
 // Create Express App
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://movie-website-chi-lemon.vercel.app', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true // Allow credentials if needed
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
